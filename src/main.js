351ccfbf6bc9c612734e12ee78a6eb6fd196fc28
wFanhua <744118724@qq.com>
2019-10-26 16:34:28 +0800

    feat(babel): babel 配置 element-ui 按需引入

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+import { Button, Select } from 'element-ui';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+Vue.component(Button.name, Button);
+Vue.component(Select.name, Select);
 
 Vue.config.productionTip = false;
 
